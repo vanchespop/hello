abe9b900f0800683e455778e93dbe90795dc5f0c
Denis <denisstep99@gmail.com>
2019-10-20 19:31:17 +0300

    just small changes

--- TOE_Monitor/scripts/background.js
+++ TOE_Monitor/scripts/background.js
@@ -1,73 +1,59 @@
+'use strict';
+
 console.log("background script");
 
-
-chrome.runtime.onMessage.addListener(
-function(message, sender, response)
-{
-
-
-
-
-
-const baseApiUrl = 'https://api.telegram.org';
-const token = '872215482:AAEzxIM77DE-yMJlAV74aU_P0tSqh_Joj_w';
-const chat_id = message.id;
-var url = "https://vk.com/board176748083";
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url, true);
-xhr.send();
-xhr.onreadystatechange = function () {
-if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-
-var data = xhr.responseText;
-
-f = new DOMParser().parseFromString(data, "text/html");
-//alert(f);
-var total = f.getElementById('blst_cont').childNodes.length;
-
-
-
-function _buildURL(token, name, text) {
-return `${baseApiUrl}/bot${token}/sendMessage?chat_id=${name}&text=${text}`;
-}
-
-function sendMessage(token, name, text) {
-	fetch(_buildURL(token, name, text))
-		.then(console.log)
-		.catch(console.log);
-}
-
-//257271396
-
-
-setInterval(function rq (){
-  var new_url = "https://vk.com/board176748083";
-  var new_xhr = new XMLHttpRequest();
-  new_xhr.open('GET', url, true);
-  new_xhr.send();
-  new_xhr.onreadystatechange = function () {
-  if(new_xhr.readyState === XMLHttpRequest.DONE && new_xhr.status === 200) {
-
-  var new_data = new_xhr.responseText;
-
-  htm = new DOMParser().parseFromString(new_data, "text/html");
-
-  var new_total = htm.getElementById('blst_cont').childNodes.length;
-  if(new_total > total){
-    total++;
-
-    var title = htm.querySelector('.blst_title').innerText;
-    var link = "https://vk.com" + htm.querySelector('.blst_title').getAttribute('href');
-    var answer = confirm("Доступно новое задание: \n" + title + "\nПерейти к обсуждению?")
-    if(answer){
-      window.open(link);
+chrome.runtime.onMessage.addListener(function (message) {
+    function _buildURL(token, name, text) {
+        return `${baseApiUrl}/bot${token}/sendMessage?chat_id=${name}&text=${text}`;
     }
-    sendMessage(token, chat_id, "Доступно новое задание: \n" + title + "                А вот и ссылОчка: " + link);
-  }
 
-  }
-}
-},5000);
-}
-}
-})
+    function sendMessage(token, name, text) {
+        fetch(_buildURL(token, name, text))
+            .then(console.log)
+            .catch(console.error);
+    }
+
+    const
+        baseApiUrl = 'https://api.telegram.org',
+        token = '872215482:AAEzxIM77DE-yMJlAV74aU_P0tSqh_Joj_w',
+        chat_id = message.id,
+        url = "https://vk.com/board176748083",
+        xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
+    xhr.send(null);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            let data = xhr.responseText,
+                parser = new DOMParser().parseFromString(data, "text/html"),
+                total = parser.getElementById('blst_cont').childElementCount;
+
+            setInterval(function () {
+                let new_xhr = new XMLHttpRequest();
+                new_xhr.open('GET', url, true);
+                new_xhr.send();
+                new_xhr.onreadystatechange = function () {
+                    if (new_xhr.readyState === XMLHttpRequest.DONE && new_xhr.status === 200) {
+                        const
+                            new_data = new_xhr.responseText,
+                            htm = new DOMParser().parseFromString(new_data, "text/html"),
+                            new_total = htm.getElementById('blst_cont').childNodes.length;
+
+                        if (new_total > total) {
+                            const
+                                title = htm.querySelector('.blst_title').innerText,
+                                link = "https://vk.com" + htm.querySelector('.blst_title').getAttribute('href'),
+                                answer = confirm(`Доступно новое задание: \n${title} \nПерейти к обсуждению?`);
+
+                            if (answer) {
+                                window.open(link);
+                            }
+                            sendMessage(token, chat_id, `Доступно новое задание: \n${title}\n\nА вот и ссылОчка:\n${link}`);
+                            total++;
+                        }
+                    }
+                }
+            }, 5000);
+        }
+    }
+});
--- TOE_Monitor/scripts/toe.js
+++ TOE_Monitor/scripts/toe.js
@@ -1,12 +1,8 @@
-
-
-document.getElementById('btn').onclick = _chatIdSuccess;
-
 function _chatIdSuccess(){
-
+  const chat_id = document.getElementById('chat').value;
   alert("Теперь ты сможешь получать уведомления в Telegram");
-  var chat_id = document.getElementById('chat').value;
   alert(chat_id);
   chrome.runtime.sendMessage({id:chat_id});
-
 }
+
+document.getElementById('btn').onclick = _chatIdSuccess;
